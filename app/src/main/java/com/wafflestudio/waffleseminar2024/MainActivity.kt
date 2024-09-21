633e98e8ef91ab6c725c2fd354a782feb2c39794
nyunn2 <jungyun_01@snu.ac.kr>
2024-09-22 00:29:47 +0900

    assignment-1

--- app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.wafflestudio.waffleseminar2024
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<TextView>(R.id.myButton)
+        val editTextUrl = findViewById<EditText>(R.id.urlEditText)
+
+        button.setOnClickListener {
+            val url = editTextUrl.text.toString()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("url_key", url)
+            startActivity(intent)
         }
     }
 }
