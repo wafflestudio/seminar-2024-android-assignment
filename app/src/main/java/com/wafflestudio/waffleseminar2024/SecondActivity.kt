45b8e354e1b11f7cb3e76bdf2ed313e1689e07ee
moo <lgmoo2002@gmail.com>
2024-09-22 18:59:00 +0900

    2nd ver

--- app/src/main/java/com/wafflestudio/waffleseminar2024/SecondActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/SecondActivity.kt
@@ -13,13 +13,14 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val textView: TextView = findViewById(R.id.textView)
+        val textView: TextView = findViewById(R.id.text_input)
 
         // 인텐트에서 데이터 추출
         val receivedText = intent.getStringExtra("EXTRA_TEXT")
 
         // TextView에 데이터 설정
-        textView.text = receivedText
+        textView.text = "내 Slack 주소 : $receivedText"
+        //"내 Slack 주소 : $receivedText" 이걸로 하면 잘됨.
 
         // 하이퍼링크 설정
         val hyperlinkTextView: TextView = findViewById(R.id.hyperlinkTextView)
