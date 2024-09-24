92d0caec9c18dd9c5a7751be0316b39c1ea944af
wowsnu <dltkddn233@snu.ac.kr>
2024-09-24 21:22:00 +0900

    우선 올림

--- app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.wafflestudio.waffleseminar2024
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.wafflestudio.waffleseminar2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val url = binding.urlInput
+        val button = binding.button
+
+        url.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                val input = s.toString()
+                button.isEnabled = input.isNotEmpty() && input.contains(".slack.com")
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        val buttonGotoSecond = binding.button
+        buttonGotoSecond.setOnClickListener {
+            val intent = Intent(this, second_activity::class.java)
+            startActivity(intent)
         }
+        }
+
     }
-}
---
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/second_activity.kt
@@ -0,0 +1,18 @@
+package com.wafflestudio.waffleseminar2024
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.wafflestudio.waffleseminar2024.databinding.ActivityMainBinding
+
+class second_activity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
