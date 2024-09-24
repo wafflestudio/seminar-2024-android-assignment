aab215fd35f39154b24d48417b9572f9d1fc033c
huiwoo1010 <57035369+huiwoo1010@users.noreply.github.com>
2024-09-24 23:50:54 +0900

    Add files via upload

---
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/SecondActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firstapp.databinding.ActivitySecondBinding
+
+class SecondActivity: AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val receivedText = intent.getStringExtra("USER_INPUT")
+        binding.linkText.text = receivedText
+    }
+}
