a8ae204580390ac11817f5a172aec697b240f242
dlcksghl159 <[dlcksghl159@gmail.com]>
2024-09-24 19:45:32 +0900

    액티비티 생성

--- app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity2.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity2.kt
@@ -1,38 +1,30 @@
 package com.wafflestudio.waffleseminar2024
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        val linkTextView: TextView = findViewById(R.id.linkTextView)
+        linkTextView.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/wafflestudio/seminar-2024"))
+            startActivity(browserIntent)
         }
-
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-
-
         }
     }
-
-
-
 }
