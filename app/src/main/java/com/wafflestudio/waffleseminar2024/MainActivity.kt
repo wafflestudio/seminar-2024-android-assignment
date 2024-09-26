9f52ab758a0681236b8f27ad1b767f1078e8722a
pcy1203 <pcy1203@snu.ac.kr>
2024-09-26 22:10:59 +0900

    Assignment 1

--- app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.wafflestudio.waffleseminar2024
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.view.View
+import android.widget.Button
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
