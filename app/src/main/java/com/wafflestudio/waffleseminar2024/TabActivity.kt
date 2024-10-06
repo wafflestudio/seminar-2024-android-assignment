b7aaad4dbf2dfebb5f47d1bdebb17ab4f61f22cd
nyunn2 <jungyun_01@snu.ac.kr>
2024-10-06 21:26:37 +0900

    profile

--- app/src/main/java/com/wafflestudio/waffleseminar2024/TabActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/TabActivity.kt
@@ -1,8 +1,16 @@
 package com.wafflestudio.waffleseminar2024
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -11,16 +19,21 @@ import com.wafflestudio.waffleseminar2024.databinding.ActivityUserInformationBin
 
 class TabActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTabBinding
+    private val userInformationBinding by lazy {
+        ActivityUserInformationBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityTabBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //userInformationBinding = ActivityUserInformationBinding.inflate(layoutInflater)
+
         val viewPager: ViewPager2 = binding.viewPager
         val tabLayout: TabLayout = binding.tabLayout
 
-        // 페이지에 표시할 데이터를 준비
         val items = listOf("Page 1", "Page 2", "Page 3", "Page 4")
         val tabIcons = listOf(
             R.drawable.ic_game,
@@ -30,21 +43,54 @@ class TabActivity : AppCompatActivity() {
         )
         val tabTexts = listOf("게임", "앱", "검색", "프로필")
 
-        // 어댑터 생성
-        val adapter = TabAdapter(items)
+        val adapter = TabAdapter(items, this)
         viewPager.adapter = adapter
 
-        // TabLayout과 ViewPager2를 연결
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = tabTexts[position]
             tab.setIcon(tabIcons[position])
         }.attach()
     }
 
-    private fun addWorkspaceUrl() {
-        val slackWorkspaceValueView: TextView = ActivityUserInformationBinding.inflate(layoutInflater).slackWorkspaceValue
+    fun addToolbarOption() {
+        val toolbar: Toolbar = userInformationBinding.toolbarUserInformation
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "프로필"
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_ios_new_24)
+    }
+
+    fun addWorkspaceUrl() {
+        val slackWorkspaceValueView: TextView = userInformationBinding.slackWorkspaceValue
         val workspaceUrl = intent.getStringExtra("WORKSPACE_URL")
+
+        Log.d("TabActivity", "url: $workspaceUrl")
         slackWorkspaceValueView.text = workspaceUrl
+
+    }
+
+    fun addGithubLink() {
+        val textView: TextView = userInformationBinding.githubValue
+        val text = "hjlim7831"
+        val spannableString = SpannableString(text)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: android.view.View) {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/hjlim7831"))
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+
+        spannableString.setSpan(clickableSpan, 0, text.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = spannableString
+        textView.movementMethod = LinkMovementMethod.getInstance()
 
     }
 }
--- app/src/main/java/com/wafflestudio/waffleseminar2024/TabAdapter.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/TabAdapter.kt
@@ -1,24 +1,49 @@
 package com.wafflestudio.waffleseminar2024
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.wafflestudio.waffleseminar2024.databinding.ActivityUserInformationBinding
 
-class TabAdapter(private val items: List<String>) : RecyclerView.Adapter<TabAdapter.ViewHolder>() {
+class TabAdapter(
+    private val items: List<String>,
+    private val activity: TabActivity
+) : RecyclerView.Adapter<TabAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textView: TextView = itemView.findViewById(R.id.textView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_viewpager_page, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = when (viewType) {
+            3 -> inflater.inflate(
+                R.layout.activity_user_information,
+                parent,
+                false
+            )
+            else -> inflater.inflate(
+                R.layout.item_viewpager_page,
+                parent,
+                false
+            )
+        }
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        Log.d("TabAdapter", "onBindViewHolder called for position: $position")
+        when (position) {
+            3 -> {
+                activity.addToolbarOption()
+                activity.addWorkspaceUrl()
+                activity.addGithubLink()
+            }
+
+            else -> holder.textView.text = items[position]
+        }
     }
 
     override fun getItemCount(): Int {
