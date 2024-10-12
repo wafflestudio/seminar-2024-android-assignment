082b5c76a3ad3e033239b096c9fd54d64c72bbcb
qdrptd <99246971+qdrptd@users.noreply.github.com>
2024-10-13 02:04:43 +0900

    제목 검색 쿼리, RecyclerViewAdapter 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.coil)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/wafflestudio/waffleseminar2024/SearchFragment.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/SearchFragment.kt
@@ -1,28 +1,23 @@
 package com.wafflestudio.waffleseminar2024
 
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.wafflestudio.waffleseminar2024.databinding.FragmentSearchBinding
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
-
+    lateinit var searchResultRecyclerView: RecyclerView
+    lateinit var genreRecyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,10 +34,32 @@ class SearchFragment : Fragment() {
 
         searchButton.text = "검색"
 
-        val recyclerView = binding.genreRecyclerView
+        genreRecyclerView = binding.genreRecyclerView
         val gridLayoutManager = GridLayoutManager(requireContext(), 2)
-        recyclerView.layoutManager = gridLayoutManager
-        recyclerView.adapter = GenreRecyclerViewAdapter(GenreList)
+        genreRecyclerView.layoutManager = gridLayoutManager
+        genreRecyclerView.adapter = GenreRecyclerViewAdapter(GenreList)
+
+        searchResultRecyclerView = binding.searchResultRecyclerView
+        val data: List<Movie> = titleQuery("e")
+
+        searchButton.setOnClickListener{
+            val data: List<Movie> = titleQuery(searchEditText.text.toString())
+            showResult(data)
+        }
+    }
+
+    private fun showResult(data: List<Movie>) {
+        searchResultRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        searchResultRecyclerView.adapter = searchResultRecyclerViewAdapter(data)
+        searchResultRecyclerView.visibility = View.VISIBLE
+        genreRecyclerView.visibility = View.INVISIBLE
+    }
+
+
+    private fun titleQuery(query: String): List<Movie>{
+        return MovieData.filter{ movie ->
+            movie.title.contains(query, ignoreCase = true)
+        }
     }
 
     override fun onDestroyView() {
