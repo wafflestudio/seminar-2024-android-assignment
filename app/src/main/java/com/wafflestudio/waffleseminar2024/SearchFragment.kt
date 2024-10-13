d85db723cd2fe4d721351d705a8daf8d067705a0
qdrptd <99246971+qdrptd@users.noreply.github.com>
2024-10-13 14:43:44 +0900

    키보드 입력

--- app/src/main/java/com/wafflestudio/waffleseminar2024/SearchFragment.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/SearchFragment.kt
@@ -1,13 +1,17 @@
 package com.wafflestudio.waffleseminar2024
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -48,6 +52,17 @@ class SearchFragment : Fragment(), OnGenreClickListener {
             showResult(data)
         }
 
+        searchEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val data: List<Movie> = titleQuery(searchEditText.text.toString())
+                showResult(data)
+                val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+                true
+            } else {
+                false
+            }
+        }
         binding.backButton.setOnClickListener{
             hideResult()
         }
